55b915615bf2e8f5ba3e137244ba4cb542c90e6a
Mejdoubee <recovery.mejdoub@gmail.com>
2023-04-27 20:37:50 +0100

    v3

--- shellheader.h
+++ shellheader.h
@@ -9,12 +9,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
 
 
 char *read_line();
-char **parse_commands(char *line);
+char **parse_commands(char *line, int *countsc);
 char *trim(char *str);
 char *get_command_path(char *command);
+void hbtn_ls_wrapper(char *command_path, char **args, char **envp);
 int execute(char *command_path, char **args, char **envp);
 void copy_ls_to_hbtn_ls();
 
